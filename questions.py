0ed1f4fa35e234d3975a98e0a809c48ad55d9543
GaurNandita2627 <gaurnandita27@gmail.com>
2026-01-22 15:55:51 +0530

    Added initial Python quiz questions
    Added the initial set of Python multiple-choice questions covering core topics like variables, loops, functions, and data structures. 
    This file will be used by quiz_app.py for running the interactive Python quiz.

---
+++ questions.py
@@ -0,0 +1,17 @@
+questions = [
+    {
+        "question": "Which of the following is a mutable data type in Python?",
+        "options": ["A. List", "B. Tuple", "C. String", "D. int"],
+        "answer": "A"
+    },
+    {
+        "question": "Which operator is used for exponentiation in Python?",
+        "options": ["A. ^", "B. **", "C. %", "D. //"],
+        "answer": "B"
+    }
+    {
+    "question": "What is the output of 2**3?",
+    "options": ["A. 6", "B. 8", "C. 9", "D. 5"],
+    "answer": "B"
+},
+]
